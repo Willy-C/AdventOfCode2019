1060df90f994b10661a37b838af74076c9795b2a
Willy <willychan@live.ca>
2019-12-02 03:46:27 -0500

    tweak

--- day2/day2.py
+++ day2/day2.py
@@ -2,7 +2,7 @@ import operator
 
 with open('input.txt', 'r') as f:
     data_list = f.readline().strip().split(',')
-    _data = [int(x) for x in data_list]
+    data = [int(x) for x in data_list]
 
 
 operations = {
@@ -11,21 +11,17 @@ operations = {
 }
 
 
-def process_code(index: int, data):
-    opcode = data[index]
-    if opcode in operations:
-        data[data[index + 3]] = operations[opcode](data[data[index + 1]], data[data[index + 2]])
-    else:
-        return
-
-
 def process_data(data: list) -> list:
     for i in range(0, len(data), 4):
-        process_code(i, data)
+        opcode = data[i]
+        if opcode in operations:
+            data[data[i+3]] = operations[opcode](data[data[i+1]], data[data[i+2]])
+        else:
+            break
     return data[0]
 
 
-print(f'Part 1: {process_data(_data.copy())}')
+print(f'Part 1: {process_data(data.copy())}')
 
 
 # Part 2
@@ -34,7 +30,7 @@ print(f'Part 1: {process_data(_data.copy())}')
 def find_pair():
     for i in range(100):
         for j in range(100):
-            copy = _data.copy()
+            copy = data.copy()
             copy[1], copy[2] = i, j
             if process_data(copy) == 19690720:
                 return i, j
